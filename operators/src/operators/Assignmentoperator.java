a5c98c282b609b95f9b0563b508a60d086da7884
Meghana014 <mullangimeghana015@gmail.com>
2022-03-22 21:23:32 +0530

    v7

---
+++ operators/src/operators/Assignmentoperator.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class Assignmentoperator {
+
+	public static void main(String[] args) {
+		int a=50,b=25;
+		System.out.println(a += b);
+		System.out.println(a -= b);      //25
+		System.out.println(a *= b);
+		System.out.println(a/b);         //2
+		System.out.println(a %= b);
+		
+
+	}
+
+}
